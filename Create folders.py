8fee30bb1192dfaa5e68d016cf132549cc7be635
Yegor Gladkov <gladkovyegor@gmail.com>
2018-01-15 16:36:55 +0400

    Empty string bug
    Now works with an empty "name"

--- Create folders.py
+++ Create folders.py
@@ -2,6 +2,9 @@ import os
 
 N = input("How mush folder do you need?\n")
 name = raw_input("Enter the name of folders: ")
-for i in range(1, N + 1):
-    os.mkdir("{0}_{1}".format(name, i))
-
+if name != "":
+    for i in range(1, N + 1):
+        os.mkdir("{0}_{1}".format(name, i))
+elif name == "":
+    for i in range(1, N + 1):
+        os.mkdir("{0}".format(i))
